e7ed92516a54af331ee1559c717dbdf34f813d56
jjdd <juanjo.dominguez.duarte@gmail.com>
2013-09-05 22:25:44 +0100

    google like index page

--- web.js
+++ web.js
@@ -5,7 +5,8 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   var html= fs.readFileSync('index.html').toString()
   var chart_data= fs.readFileSync('country_chart.json').toString()
-  var table_data= fs.readFileSync('table.json').toString()
+  var table_json= JSON.parse(fs.readFileSync('table.json').toString())
+  var table_data= JSON.stringify([table_json[0]])//send only first element
   var html= html.replace('CHART_DATA',chart_data)
   var html= html.replace('TABLE_DATA',table_data) 
   response.send(html);

7f154d68ef39dfe34a0f7c5bf875b832c452246e
jjdd <juanjo.dominguez.duarte@gmail.com>
2013-09-06 22:48:12 +0100

    poniendo al dia

--- web.js
+++ web.js
@@ -2,6 +2,11 @@ var express = require('express');
 var fs = require('fs')
 var app = express.createServer(express.logger());
 
+var table_data;
+var chart_data;
+
+app.use(express.bodyParser());
+
 app.get('/', function(request, response) {
   var html= fs.readFileSync('index.html').toString()
   var chart_data= fs.readFileSync('country_chart.json').toString()
@@ -11,6 +16,12 @@ app.get('/', function(request, response) {
   var html= html.replace('TABLE_DATA',table_data) 
   var html= html.replace('CHAMPION',table_json[0][2]+', '+table_json[0][3]) 
   response.send(html);
+});
+
+app.post('/set_table', function(request, response) {
+  table_data= JSON.parse(request.body.table);
+  console.log(table_data);
+  response.send(JSON.stringify(table_data));
 });
 
 var port = process.env.PORT || 8000;

a9e1f4870521ad94c8ab6ccee7bb9d53b7450d74
jjdd <juanjo.dominguez.duarte@gmail.com>
2013-09-08 02:26:12 +0100

    tooltip,iata/icao optional

--- web.js
+++ web.js
@@ -20,7 +20,7 @@ var NWPStyle= function(nwp){
 
 var setNWP= function(data){
 	for (i in data){
-		data[i]= [NWPStyle(data[i][0])].concat(data[i].slice(1,11));
+		data[i]= [NWPStyle(data[i][0])].concat(data[i].slice(1));
 		}
 	return data;
 	};
@@ -34,23 +34,29 @@ var collapseSpaces= function(term){
 
 var getSelection= function(term,data){
 	var result= [];
+	var table_columns= {iata: false, icao: false};
+	var search_term= term.toLowerCase();
 	for (i in data){
-		var found= data[i].slice(1,4).reduce(function(previous,x){return previous || x.search(term)>=0},false);
+		var found= data[i].slice(1,5).reduce(function(previous,x){return previous || x.toLowerCase().search(search_term)>=0},false);
+		table_columns.iata= table_columns.iata || data[i][1].search(term)>=0;
+		table_columns.icao= table_columns.icao || data[i][2].search(term)>=0;
 		//console.log([term,found,data[i].slice(1,4)]);
-		if (found) result.push(data[i].slice(0,1).concat(data[i].slice(1,4).map(function(x){return x.replace(term,'<strong>'+term+'</strong>')}),data[i].slice(4,11)));	
+		if (found) result.push(data[i].slice(0,1).concat(data[i].slice(1,5).map(function(x){return x.replace(term,'<strong>'+term+'</strong>')}),data[i].slice(5)));	
 	}
-	return result;
+	return {data:result,columns:table_columns};
 };
 
 app.get('/', function(request, response) {
   var html= fs.readFileSync('index.html').toString();
   var chart_data= fs.readFileSync('country_chart.json').toString();
   var table_json= JSON.parse(fs.readFileSync('table.json').toString());
-  var table_data= JSON.stringify(setNWP([table_json[0]]));//send only first element
+  var champion= [[table_json[0][0]].concat(table_json[0].slice(3))];//remove iata,icao by default
+  var table_data= JSON.stringify(setNWP(champion));//send only first element
   console.log(table_data);
-  var html= html.replace('CHART_DATA',chart_data);
-  var html= html.replace('TABLE_DATA',table_data);
-  var html= html.replace('CHAMPION',table_json[0][2]+', '+table_json[0][3]); 
+  html= html.replace('CHART_DATA',chart_data);
+  html= html.replace('TABLE_DATA',table_data);
+  html= html.replace('CHAMPION',table_json[0][3]+', '+table_json[0][4]);//city and country 
+  html= html.replace('NUM_LOCATIONS',table_json.length);   
   response.send(html);
 });
 
@@ -62,15 +68,25 @@ app.post('/', function(request, response) {
   
   //collapse spaces, heading and trailing 
   var search_term= collapseSpaces(request.body.search)
-  if (search_term)
-  	  table_data= JSON.stringify(setNWP(getSelection(search_term,table_json)));//filter
-  else
-    table_data= JSON.stringify(setNWP(table_json));//send all elements
-    
-  var html= html.replace('CHART_DATA',chart_data);
-  var html= html.replace('TABLE_DATA',table_data);
-  var html= html.replace('CHAMPION',table_json[0][2]+', '+table_json[0][3]); 
-  response.send(html);
+  var table_columns= JSON.stringify();//remove by default
+  var selected;   
+  if (search_term)  selected= getSelection(search_term,table_json); //filter
+  else selected= {data:table_json, columns:{iata:false, icao:false}};
+  
+  selected.data= selected.data.map(function(x){
+  	  		if (!selected.columns.iata && !selected.columns.icao) x=[x[0]].concat(x.slice(3));
+  	  		if (!selected.columns.iata &&  selected.columns.icao) x=[x[0]].concat(x.slice(2));
+  	  		if ( selected.columns.iata && !selected.columns.icao) x=x.slice(0,2).concat(x.slice(3));
+  	  		return x;  	  			
+  });// remove the unused columns
+
+  var table_data= JSON.stringify(setNWP(selected.data));
+  var table_columns= JSON.stringify(selected.columns);
+  html= html.replace('TABLE_DATA',table_data);
+  html= html.replace('TABLE_COLUMNS',table_columns);    
+  html= html.replace('SEARCH_TERM',search_term);  
+  html= html.replace('NUM_LOCATIONS',selected.data.length); 
+  response.send(html);  
 });
 
 
